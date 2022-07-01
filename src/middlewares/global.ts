784cdf072ef325731a7401e15221372fb9fb6b93
Roseul Junior <roseuljunior@gmail.com>
2022-06-30 23:57:47 -0300

    cria middleware global

--- src/middlewares/global.ts
+++ src/middlewares/global.ts
@@ -2,10 +2,16 @@
 //Se a requisição for inválida baseada no token ou em qualquer outro parâmetro (o mais comum é validação com isempty (flutter) ou req !== null (Typescript / Javascript);
 
 import { Request, Response, NextFunction } from 'express';
+import { HttpBadRequestCode } from '../constants/http';
+import { invalidField } from '../constants/messages';
+import { HttpError } from '../errors/httpErrors';
 
-export const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const { ip, method } = req;
-    console.log(ip, method);
+export const errandsMiddlewares = (request: Request, response: Response, next: NextFunction) => {
+    const { description, details } = request.body;
+
+    if (!description || !details || description.length < 3 || description.length > 255 || details.length < 3 || details.length > 255) {
+        throw new HttpError(invalidField('Nome'), HttpBadRequestCode);
+    }
     
     next();
 };
